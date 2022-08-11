9781049d54c5a32a4d4fc81471af938bde43e6b3
ptbs <270354009@qq.com>
2022-08-12 01:07:37 +0800

    增加de-emphasis.py 去重脚本

--- de-emphasis.py
+++ de-emphasis.py
@@ -4,3 +4,38 @@
 # @File    : de-emphasis.py
 # @Software: PyCharm
 # ---------CODE-------------
+def openfile():
+    filename = input("请输入文件路径：")
+    try:
+        f = open(filename,"r",encoding="utf-8")
+        content = f.readlines()
+        f.close()
+        dict = []
+        for i in range(0,len(content)):
+            dict.append(content[i].rstrip('\n'))
+        # print(dict)
+        emphasis = []
+        for i in dict:
+            if i not in emphasis:
+               emphasis.append(i)
+               write_file(i)
+        # print(emphasis)
+    except FileNotFoundError:
+        print("文件没找到.")
+    except PermissionError:
+        print("你没有权限访问此文件.")
+
+def write_file(word):
+    import os,time,calendar
+    ts = calendar.timegm(time.gmtime())
+    dirs = "./tmp"
+    if not os.path.exists(dirs):
+        os.mkdir(dirs)
+    else:
+        f = open('./tmp/tmp' + str(ts) + '.txt','a',encoding="utf-8")
+        f.write(word + "\n")
+        f.close()
+
+
+if __name__ == '__main__':
+    openfile()
